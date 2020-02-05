956470202457ca099af8b43e5693b7f6ec4e78ce
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 13:35:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,5 +3,17 @@ class Artist
   
   @@all = []
   
+  def initialize (name)
+    @name = name
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
   
 end
